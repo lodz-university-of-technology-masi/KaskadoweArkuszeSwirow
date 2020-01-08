4e61b0fb81fcf279f39f7a76043859da61ceb002
Karbowskii <arturkarbowskitest@gmail.com>
2020-01-08 13:29:18 +0100

    ***

--- frontend/src/app/test-add-question-dialog/test-add-question-dialog.component.ts
+++ frontend/src/app/test-add-question-dialog/test-add-question-dialog.component.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material';
 import { FormBuilder } from '@angular/forms';
 import { DataSource } from '@angular/cdk/table';
+import { Question } from '../models/Question.model';
 
 @Component({
   selector: 'app-test-add-question-dialog',
@@ -18,22 +19,45 @@ export class TestAddQuestionDialogComponent implements OnInit {
     throw new Error("Method not implemented.");
   }
 
-  newTestForm;
+  questions: Question[] = [];
 
   constructor(
     public dialogRef: MatDialogRef<TestAddQuestionDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Test,
-    private formBuilder: FormBuilder,
     private http: HttpClient,
-    private refresher: RefresherService
+    //private refresher: RefresherService
   ) {
-    this.newTestForm = this.formBuilder.group({
-      title: '',
-    });
+    this.getQuestions();
   }
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  addToList(data: Object): void {
+    let i = 0;
+    while (true) {
+      if (data[i] !== undefined) {
+        const question: Question = {id: data[i].id, question: data[i].question, answer: data[i].answer};
+        this.questions.push(question);
+      } else {
+        break;
+      }
+      i++;
+    }
+    console.log(this.questions);
+    //this.ref.tick();
+  }
+
+  getQuestions(): void {
+    this.http.get('https://kn0z5zq8j2.execute-api.us-east-1.amazonaws.com/new/question',
+      ).subscribe(
+      res => {
+        //this.refresher.questionRefreshSubject$.next(1);
+        this.addToList(res);
+        console.log(res);
+      }, err => console.log(err)
+    );
   }
 
   onSubmit(customerData) {
@@ -44,7 +68,7 @@ export class TestAddQuestionDialogComponent implements OnInit {
     this.http.post('https://kn0z5zq8j2.execute-api.us-east-1.amazonaws.com/new/tests',
       {'title': title, 'questions': questions}).subscribe(
       res => {
-        this.refresher.questionRefreshSubject$.next(1);
+        //this.refresher.questionRefreshSubject$.next(1);
         console.log(res);
       }, err => console.log(err)
     );
