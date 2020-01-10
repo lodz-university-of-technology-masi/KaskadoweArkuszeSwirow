89f13994b3a7d0aa831756906346b82b2e56e37b
Jakub Nozderka <jaktotowiem@o2.pl>
2020-01-10 05:04:18 +0100

    Simple form, not finished

--- frontend/src/app/solve-test/solve-test.component.ts
+++ frontend/src/app/solve-test/solve-test.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';  
 import { Test } from '../models/Test.model';
 import { HttpClient } from '@angular/common/http';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-solve-test',
@@ -9,10 +10,19 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SolveTestComponent implements OnInit {
   test: Test;
+  testForm;
 
   constructor(
-    private http: HttpClient) { 
+    private http: HttpClient,
+    private formBuilder: FormBuilder
+    ) { 
       this.getTestWithID();
+      this.testForm = this.formBuilder.group({
+        answerA: '',
+        answerB: '',
+        answerC: '',
+        answerD: ''
+      });
     }
 
   ngOnInit() {
@@ -24,7 +34,7 @@ export class SolveTestComponent implements OnInit {
   }
 
   getTestWithID(): void {
-    this.http.get(`https://kn0z5zq8j2.execute-api.us-east-1.amazonaws.com/new/tests/9b857c41-892c-4217-8baf-aea3e53f574c`)
+    this.http.get(`https://kn0z5zq8j2.execute-api.us-east-1.amazonaws.com/new/tests/f460124e-846a-41c0-8add-420b9877dd82`)
       .subscribe(data => {
         if (!('errorMessage' in data)){
           this.setTest(data);
