77abfdb95202f4de2e2ca2c6788346bfee28fa3f
Jakub Nozderka <jaktotowiem@o2.pl>
2020-01-19 19:12:45 +0100

    tlumaczenie testu

--- frontend/src/app/recruiter/tests-for-users/tests-for-users.component.ts
+++ frontend/src/app/recruiter/tests-for-users/tests-for-users.component.ts
@@ -5,6 +5,8 @@ import { User } from 'src/app/models/User.model';
 import { MatDialog } from '@angular/material';
 import { ChooseTestDialogComponent } from './choose-test-dialog/choose-test-dialog.component';
 import { HttpClient } from '@angular/common/http';
+import { Test } from 'src/app/models/Test.model';
+import { codes } from 'src/codes';
 
 @Component({
   selector: 'app-tests-for-users',
@@ -28,10 +30,10 @@ export class TestsForUsersComponent implements OnInit {
           for (let it of data) {
             this.users.push({
               'id' : it.Username, 
-              'name' : it.Attributes[0].Value,
-              'surname' : it.Attributes[3].Value, 
-              'email' : it.Attributes[4].Value, 
-              'role' : it.Attributes[1].Value
+              'name' : it.attributes[0].Value,
+              'surname' : it.attributes[3].Value, 
+              'email' : it.attributes[4].Value, 
+              'role' : it.attributes[1].Value
             });
          }
         }
@@ -41,9 +43,10 @@ export class TestsForUsersComponent implements OnInit {
   assignTest(user){ 
     const dialogRef = this.dialog.open(ChooseTestDialogComponent);
     dialogRef.afterClosed().subscribe(result => {
-      console.log(result);
+      // console.log(result);
       if (result) {
         for (let it of result) {
+          this.translate(it.testForm)
           this.http.post('https://kn0z5zq8j2.execute-api.us-east-1.amazonaws.com/new/candidateform', {
            'candidateId': user.id, 'testStatus': 'new', 'testResult': null, 'testForm': {'id': it.id, 'title': it.title, 'questions': it.questions}
           }).subscribe( res => {
@@ -52,6 +55,28 @@ export class TestsForUsersComponent implements OnInit {
          );}
       }
     });
+  }
+
+  translate(test: any) {
+    let textToTranslate: String = '';
+    textToTranslate = test.title + ';';
+    for(let question of test.questions) {
+      textToTranslate += question.question + ';';
+      if (question.type == "O") {
+        textToTranslate += ";;;;";
+      } else if (question.type == "L") {
+        textToTranslate += question.answer[0].content + ';;;';
+      } else if (question.type == "W") {
+        for(let answer of question.answer) {
+          textToTranslate += answer.content + ';';
+        }
+      }
+    }
+    // console.log(textToTranslate);
+    this.http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=' + codes.YANDEX_API_KEY + '&text=' + textToTranslate +'&lang=en&format=html')
+      .subscribe(data => {
+        console.log(data)
+      })
   }
 
 }
